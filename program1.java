0eee5bef0a5f8eca7f80ca6499be336de88b18f4
Shivani Oswal <shivanimoswal@gmail.com>
2023-11-06 15:08:49 +0530

    practice code

---
+++ program1.java
@@ -0,0 +1,21 @@
+/* Write a Java Program to add 3 numbers */
+
+public class program1
+{
+	int add(int num1,int num2,int num3)
+	{
+		int addition=num1+num2+num3;
+		System.out.println("Addition of numbers is:"+addition);
+		return addition;
+	}
+
+
+	
+	public static void main(String[] args)
+	{
+		program1 p=new program1();
+		p.add(1,2,3);
+	}
+}
+O/P --> Addition of numbers is:6
+		
---
+++ program2.java
@@ -0,0 +1,25 @@
+/* Write a program to take 3 numbers from the keyboard and display addition */
+
+import java.util.*;
+public class program2
+{
+	int add(int num1,int num2, int num3)
+	{
+		int addition=num1+num2+num3;
+		 System.out.println("Addition is:-"+addition);
+		 return addition;
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 3 numbers:-");
+		 int num1=sc.nextInt();
+		 int num2=sc.nextInt();
+		 int num3=sc.nextInt();
+		 program2 p=new program2();
+		 p.add(num1, num2, num3);
+		 sc.close();
+		 
+		 
+	}
+}
